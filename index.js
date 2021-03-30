2470e7c6a44c4a7bdc79fb9f3299eab5f9699fda
yavorant <yavorant@gmail.com>
2021-03-30 16:34:29 +0300

    Add different sound

--- index.js
+++ index.js
@@ -26,8 +26,33 @@ document.querySelectorAll('button').forEach(item => {
 //Angela's method
 const howManyButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < howManyButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener('click', event => {
-    new Audio('sounds/tom-1.mp3').play();
+  document.querySelectorAll(".drum")[i].addEventListener('click', function() {
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case 'w':
+        new Audio('sounds/tom-1.mp3').play();
+        break;
+      case 'a':
+        new Audio('sounds/tom-2.mp3').play();
+        break;
+      case 's':
+        new Audio('sounds/tom-3.mp3').play();
+        break;
+      case 'd':
+        new Audio('sounds/tom-4.mp3').play();
+        break;
+      case 'j':
+        new Audio('sounds/crash.mp3').play();
+        break;
+      case 'k':
+        new Audio('sounds/kick-bass.mp3').play();
+        break;
+      case 'l':
+        new Audio('sounds/snare.mp3').play();
+        break;
+    }
+    //this.style.color = "white";
+
     //alert("Good job!");
   })
 }
