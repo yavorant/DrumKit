d5317a8b66538d49ed604a848776d02aa1a9939a
yavorant <yavorant@gmail.com>
2021-04-03 16:39:34 +0300

    170: KeyBoard event

--- index.js
+++ index.js
@@ -24,35 +24,46 @@ document.querySelectorAll('button').forEach(item => {
 
 */
 //Angela's method
+
+//Detecting Button Press
 const howManyButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < howManyButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener('click', function() {
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case 'w':
-        new Audio('sounds/tom-1.mp3').play();
-        break;
-      case 'a':
-        new Audio('sounds/tom-2.mp3').play();
-        break;
-      case 's':
-        new Audio('sounds/tom-3.mp3').play();
-        break;
-      case 'd':
-        new Audio('sounds/tom-4.mp3').play();
-        break;
-      case 'j':
-        new Audio('sounds/crash.mp3').play();
-        break;
-      case 'k':
-        new Audio('sounds/kick-bass.mp3').play();
-        break;
-      case 'l':
-        new Audio('sounds/snare.mp3').play();
-        break;
-    }
+    makeSound(buttonInnerHTML);
     //this.style.color = "white";
 
     //alert("Good job!");
   })
+}
+
+//Detecting Keyboard Press
+document.addEventListener('keydown', function(event) {
+    makeSound(event.key)
+});
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      new Audio('sounds/tom-1.mp3').play();
+      break;
+    case 'a':
+      new Audio('sounds/tom-2.mp3').play();
+      break;
+    case 's':
+      new Audio('sounds/tom-3.mp3').play();
+      break;
+    case 'd':
+      new Audio('sounds/tom-4.mp3').play();
+      break;
+    case 'j':
+      new Audio('sounds/crash.mp3').play();
+      break;
+    case 'k':
+      new Audio('sounds/kick-bass.mp3').play();
+      break;
+    case 'l':
+      new Audio('sounds/snare.mp3').play();
+      break;
+  }
 }
